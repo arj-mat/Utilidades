35d5fe7b001fa2196d729febcc11c969fd59d0e1
Mateus <mtsgomes.ar@gmail.com>
2022-02-18 11:07:16 -0300

    Create $chain.js

---
+++ $chain.js
@@ -0,0 +1,25 @@
+function $chain(target) {
+    let methods = {
+        $unchain: () => target
+    };
+    
+    for(let method of Object.getOwnPropertyNames(target.constructor.prototype)) 
+       if (typeof target[method] == 'function')
+            methods[method] = (...args) => {
+                target[method](...args); return methods
+            };
+
+    return methods;
+}
+
+// Exemplo:
+
+$chain(new Date())
+    .setHours(0)
+    .setMinutes(0)
+    .setSeconds(0)
+    .setMilliseconds(0)
+    .$unchain()
+    .getTime();
+
+// 1645153200000
